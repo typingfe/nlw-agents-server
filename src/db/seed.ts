dd58b7c5b10cbfc97ffcc4d82336eef4c6c5a1e8
Felipe <feelipe.ss31@gmail.com>
2025-07-12 17:14:11 -0300

    add upload audio and gemini  comunnication

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -7,14 +7,14 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
     return {
         rooms: {
-            count: 25,
+            count: 5,
             columns: {
                 name: f.companyName(),
                 description: f.loremIpsum(),
             },
         },
         questions: {
-            count: 25,
+            count: 5,
         },
     }
 })
--- src/http/routes/create-question.ts
+++ src/http/routes/create-question.ts
@@ -1,8 +1,9 @@
+import { and, eq, sql } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
-
 import { z } from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { generateAnswer, generateEmbeddings } from '../../services/gemini.ts'
 
 export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
     app.post(
@@ -21,11 +22,42 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
             const { roomId } = params
             const { question } = body
 
+            const embeddings = await generateEmbeddings(question)
+            const embeddingAsString = `[${embeddings.join(',')}]`
+
+            const chunks = await db
+                .select({
+                    id: schema.audioChunks.id,
+                    transcription: schema.audioChunks.transcription,
+                    similarity: sql<number>`1 - (${schema.audioChunks.embeddings} <=> ${embeddingAsString}::vector)`,
+                })
+                .from(schema.audioChunks)
+                .where(
+                    and(
+                        eq(schema.audioChunks.roomId, roomId),
+                        sql`1 - (${schema.audioChunks.embeddings} <=> ${embeddingAsString}::vector) > 0.7`
+                    )
+                )
+                .orderBy(sql`${schema.audioChunks.embeddings} <=> ${embeddingAsString}::vector`)
+                .limit(3)
+
+            // biome-ignore lint/suspicious/noConsole: <test>
+            console.log(chunks)
+
+            let answer: string | null = null
+
+            if (chunks.length > 0) {
+                const transcriptions = chunks.map((chunk) => chunk.transcription)
+
+                answer = await generateAnswer(question, transcriptions)
+            }
+
             const result = await db
                 .insert(schema.questions)
                 .values({
                     roomId,
                     question,
+                    answer,
                 })
                 .returning()
 
@@ -35,7 +67,7 @@ export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
                 throw new Error('Failed to create a new question')
             }
 
-            return reply.status(201).send({ roomId: insertedQuestion.id })
+            return reply.status(201).send({ questionId: insertedQuestion.id, answer })
         }
     )
 }
